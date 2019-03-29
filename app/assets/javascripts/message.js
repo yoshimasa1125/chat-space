cf6ca7a3e7976f40e3376993c6d3012e4ec1ef3b
yoshimasa1125 <yoshimasahosoi1125@gmail.com>
2019-03-29 14:21:20 +0900

    animateの実装前

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,26 @@
 // 参照コード
 $(function() {
+  function buildHTML(message){
+    var html = `<div class="main-message__box">
+                  <div class="upperbox">
+                    <div class="upperbox__talker">
+                      ${message.user_name}
+                    </div>
+                    <div class="upperbox__date">
+                      ${message.created_at}
+                    </div>
+                   </div>
+                  <div class="text">
+                     <% if message.content.present? %>
+                        <% p.lower-message__content %>
+                            ${message.content}
+                  = image_tag message.image.url, class: 'lower-message__image' if message.image.present?`
+
+    return html;
+  }
   $("#new_message").on('submit', function(e) {
   	e.preventDefault();
-    console.log(this)
     var formData = new FormData(this);
-
     var url = $(this).attr('action')
     $.ajax({
       url: url,
@@ -13,5 +29,17 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false,
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main-message').append(html)
+      $('#new_message').val('')
+    })
   });
 });
+
+
+
+
+
+
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,3 +1,4 @@
-json.text  @comment.text
-  json.user_id  @comment.user.id
-  json.user_name  @comment.user.nickname
+json.content  @message.content
+json.user_id  @message.user_id
+json.user_name  @message.user.name
+json.created_at @message.created_at("%Y/%m/%d %H:%M")
